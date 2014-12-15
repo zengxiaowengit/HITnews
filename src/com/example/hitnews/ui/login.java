7b5fdbf0f12a54a10ce031e98795a89f52cd7fcc
= <=>
2014-12-15 10:42:56 +0800

    AddBackgroundFinshPushFunction

---
+++ src/com/example/hitnews/ui/login.java
@@ -0,0 +1,30 @@
+package com.example.hitnews.ui;
+import java.util.HashMap;
+
+import com.example.hitnews.R;
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.ViewSwitcher;
+
+
+public class login extends ActionBarActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		
+	}
+
+}
